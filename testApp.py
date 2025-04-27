c65947fb9a7803a2dd33c58e6a8ec8d090a73987
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2025-04-27 14:18:47 +0530

    Update testApp.py

--- testApp.py
+++ testApp.py
@@ -180,7 +180,7 @@ transform = transforms.Compose([
         #transforms.RandomRotation(degrees=30),
         #transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         #transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.8, 1.2)),
-        #transforms.ToTensor(),
+        transforms.ToTensor(),
         #transforms.Normalize((0.5,), (0.5,)) # Normalize Gray-scale
         #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize RGB (Type 1)
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize for RGB (Type 2)
