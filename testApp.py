6bc62b620a2791bf9dc7706aa4a3aa3ae5296f1d
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2025-04-27 14:08:48 +0530

    Update testApp.py

--- testApp.py
+++ testApp.py
@@ -174,13 +174,13 @@ model.eval()
 
 # Define the image transformations
 transform = transforms.Compose([
-    transforms.Resize((40, 40)),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomVerticalFlip(p=0.3),
-        transforms.RandomRotation(degrees=30),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.8, 1.2)),
-        transforms.ToTensor(),
+        transforms.Resize((40, 40)),
+        #transforms.RandomHorizontalFlip(p=0.5),
+        #transforms.RandomVerticalFlip(p=0.3),
+        #transforms.RandomRotation(degrees=30),
+        #transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+        #transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.8, 1.2)),
+        #transforms.ToTensor(),
         #transforms.Normalize((0.5,), (0.5,)) # Normalize Gray-scale
         #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize RGB (Type 1)
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize for RGB (Type 2)
