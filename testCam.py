a4a7c50671371c79b59af37931f767f885b7f6e0
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2025-05-13 20:55:46 +0530

    Update testCam.py

--- testCam.py
+++ testCam.py
@@ -1,7 +1,14 @@
+import asyncio
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
+
+# Ensuring asyncio loop is running
+asyncio.get_event_loop().run_until_complete(asyncio.sleep(0))
 
 def video_frame_callback(frame):
     return frame
 
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+webrtc_streamer(
+    key="example", 
+    video_frame_callback=video_frame_callback
+)
